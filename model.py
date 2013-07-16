8b5cab701d74348434bff31361abf1cc33e11cea
Grace Wong <gwongz@gmail.com>
2013-07-15 18:06:48 -0700

    Initial commit

--- model.py
+++ model.py
@@ -1,17 +1,85 @@
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy import create_engine
-from sqlalchemy import Column, Integer, String
+from sqlalchemy import Column, Integer, String, Date 
+from sqlalchemy.orm import sessionmaker
+
+
+ENGINE = None
+Session = None
 
 Base = declarative_base()
 
 ### Class declarations go here
 
+#This class = User table and inherits from Base in SQLALCHEMY
+class User(Base):
+    __tablename__ = "users"
+
+    id = Column(Integer, primary_key = True)
+    email = Column(String(64), nullable = True)
+    password = Column(String(64), nullable = True)
+    age = Column(Integer, nullable = True)
+    zipcode = Column(String(15), nullable = True)
+    gender = Column(String(1))
+    occupation = Column(String(64))
+
+    #   We don't need Init in our classes 
+    # def __init__(self, id, age, zipcode, email = None, password = None):     
+    #     self.email = email
+    #     self.password = password
+    #     self.age = age
+    #     self.zipcode = zipcode
+
+
+#This class = Movie table and inherits from Base in SQLALCHEMY
+class Movie(Base):
+    __tablename__= "movies"
+
+    id = Column(Integer, primary_key = True)
+    name = Column(String(64))
+    released_at = Column(Date)
+    imbd_url = Column(String(96))
+
+
+    #   We don't need Init in our classes 
+    # def __init__(self, name, released_at, imbd_url):  
+    #     self.name = name
+    #     self.released_at = released_at
+    #     self.imbd_url = imbd_url
+
+
+
+#This class = Ratings table and inherits from Base in SQLALCHEMY
+class Rating(Base):
+    __tablename__ = "ratings"
+
+    id = Column(Integer, primary_key = True)
+    movie_id = Column(Integer)
+    user_id = Column(Integer)
+    rating = Column(Integer)
+
+
+    #   We don't need Init in our classes 
+
+    # def __init__(self, movie_id, user_id, rating):
+    #     self.movie_id = movie_id
+    #     self.user_id = user_id
+    #     self.rating = rating
 
 ### End class declarations
+def connect():
+    global ENGINE
+    global Session
+
+    ENGINE = create_engine("sqlite:///ratings.db", echo=True)
+    Session = sessionmaker(bind=ENGINE)
+
+    return Session()
 
 def main():
-    """In case we need this for something"""
     pass
 
 if __name__ == "__main__":
     main()
+
+
--- seed.py
+++ seed.py
@@ -1,13 +1,34 @@
 import model
 import csv
+from sqlalchemy import Date
+import datetime
+
 
 def load_users(session):
-    # use u.user
-    pass
+    with open ('seed_data/u.user') as csvfile:
+        user_info = csv.reader(csvfile, delimiter ='|')
+        for row in user_info:
+            user = model.User(id = row[0], 
+                            age = row[1], 
+                            gender = row[2], 
+                            occupation = row[3],
+                            zipcode = row[4])
+            session.add(user)
+        session.commit()
 
 def load_movies(session):
+    with open ('seed_data/u.item') as csvfile:
+        movie_info = csv.reader(csvfile, delimiter = '|', quotechar = " " )
+        for row in movie_info:
+            movie = model.Movie(id = row[0],
+                                name = row[1],
+                                released_at = int(datetime.date(row[2])),
+                                imbd_url = row[4])
+            session.add(movie)
+        session.commit()
+
     # use u.item
-    pass
+    
 
 def load_ratings(session):
     # use u.data
@@ -15,7 +36,10 @@ def load_ratings(session):
 
 def main(session):
     # You'll call each of the load_* functions with the session as an argument
-    pass
+    session = model.connect()
+    # load_users(session)
+    load_movies(session)
+    # load_ratings(session)
 
 if __name__ == "__main__":
     s= model.connect()
